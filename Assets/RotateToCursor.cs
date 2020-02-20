d4ba3d5c121bdbf2f1ffad929fc7c96b4f36464f
Alex Test <admin@docode.ru>
2020-02-20 19:59:09 +0300

    rotate to cursor

---
+++ Assets/RotateToCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToCursor : MonoBehaviour
+{
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var point = cam.ScreenToWorldPoint( Input.mousePosition );
+        Utils.SetAxisTowards(Enums.Directions.Up, transform, point - transform.position);
+    }
+}
